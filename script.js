f2af78ffc388386fed12a3d1bb4721e2010713b4
Raelinda Shoates <redfire809@gmail.com>
2021-06-05 17:53:32 -0400

    added btn variables for each btn

---
+++ script.js
@@ -0,0 +1,21 @@
+const clearBtn = document.getElementById('clear');
+const signBtn = document.getElementById('sign');
+const percentBtn = document.getElementById('percentage');
+const divideBtn = document.getElementById('divide');
+const multiplyBtn = document.getElementById('multiply');
+const subtractBtn = document.getElementById('subtract');
+const addBtn = document.getElementById('addition');
+const equalBtn = document.getElementById('equals');
+const decimalBtn = document.getElementById('decimal');
+const zeroBtn = document.getElementById('zero');
+const oneBtn = document.getElementById('one');
+const twoBtn = document.getElementById('two');
+const threeBtn = document.getElementById('three');
+const fourBtn = document.getElementById('four');
+const fiveBtn = document.getElementById('five');
+const sixBtn = document.getElementById('six');
+const sevenBtn = document.getElementById('seven');
+const eightBtn = document.getElementById('eight');
+const nineBtn = document.getElementById('nine');
+
+
