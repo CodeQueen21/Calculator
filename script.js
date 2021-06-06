3bd31717891ddc5c69b0f8dce909be88d9d9c6ef
Raelinda Shoates <redfire809@gmail.com>
2021-06-06 11:50:24 -0400

    created event listeners for numbers and added functions to help display numbers correctly on the display

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const results = document.querySelector('content').querySelector('p');
+const results = document.getElementById('display').querySelector('p');
 const clearBtn = document.getElementById('clear');
 const signBtn = document.getElementById('sign');
 const percentBtn = document.getElementById('percentage');
@@ -18,4 +18,78 @@ const sixBtn = document.getElementById('six');
 const sevenBtn = document.getElementById('seven');
 const eightBtn = document.getElementById('eight');
 const nineBtn = document.getElementById('nine');
+let userInput = [];
+let number;
+
+function joinArr(arr) {
+    let joined = arr.join('');
+    return joined;
+}
+
+const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+oneBtn.addEventListener('click', () => {
+    number = 1;
+    userInput.push(number);
+    results.textContent = numberWithCommas(joinArr(userInput));
+})
+
+twoBtn.addEventListener('click', () => {
+    number = 2;
+    userInput.push(number);
+    results.textContent = numberWithCommas(joinArr(userInput));
+})
+
+threeBtn.addEventListener('click', () => {
+    number = 3;
+    userInput.push(number);
+    results.textContent = numberWithCommas(joinArr(userInput));
+})
+
+fourBtn.addEventListener('click', () => {
+    number = 4;
+    userInput.push(number);
+    results.textContent = numberWithCommas(joinArr(userInput));
+})
+
+fiveBtn.addEventListener('click', () => {
+    number = 5;
+    userInput.push(number);
+    results.textContent = numberWithCommas(joinArr(userInput));
+})
+
+sixBtn.addEventListener('click', () => {
+    number = 6;
+    userInput.push(number);
+    results.textContent = numberWithCommas(joinArr(userInput));
+})
+
+sevenBtn.addEventListener('click', () => {
+    number = 7;
+    userInput.push(number);
+    results.textContent = numberWithCommas(joinArr(userInput));
+})
+
+eightBtn.addEventListener('click', () => {
+    number = 8;
+    userInput.push(number);
+    results.textContent = numberWithCommas(joinArr(userInput));
+})
+
+nineBtn.addEventListener('click', () => {
+    number = 9;
+    userInput.push(number);
+    results.textContent = numberWithCommas(joinArr(userInput));
+})
+
+zeroBtn.addEventListener('click', () => {
+    number = 0;
+    userInput.push(number);
+    results.textContent = numberWithCommas(joinArr(userInput));
+})
+ 
+
+
 
