4448f95f6381c76e4b8e31613270eec9bdbe3708
Raelinda Shoates <redfire809@gmail.com>
2021-06-06 20:07:42 -0400

    added functionalty to the divide and equal btns

--- script.js
+++ script.js
@@ -19,7 +19,37 @@ const sevenBtn = document.getElementById('seven');
 const eightBtn = document.getElementById('eight');
 const nineBtn = document.getElementById('nine');
 let userInput = [];
-let number;
+let number = 0;
+let firstUserInput;
+let secondUserInput;
+let content;
+let secondContent;
+let answer;
+
+function divide(a,b) {
+    return a / b;
+}
+/*
+function numberChecker(arr) {
+    let count = 0;
+    for(let i = 0; i <= arr.length; i++) {
+      if(typeof arr[i] === 'number') {
+         count++;
+     }
+  }
+  return count;
+}
+
+function decimalChecker(arr) {
+    let count = 0;
+    for(let i = 0; i <= arr.length; i++) {
+        if(typeof arr[i] === 'string') {
+            count ++;
+        }
+    }
+}
+*/
+
 
 function joinArr(arr) {
     if (arr.length > 9){
@@ -36,63 +66,88 @@ const numberWithCommas = (x) => {
 oneBtn.addEventListener('click', () => {
     number = 1;
     userInput.push(number);
-    results.textContent = numberWithCommas(joinArr(userInput));
+    content = numberWithCommas(joinArr(userInput));
+    results.textContent = content;
 })
 
 twoBtn.addEventListener('click', () => {
     number = 2;
     userInput.push(number);
-    results.textContent = numberWithCommas(joinArr(userInput));
+    content = numberWithCommas(joinArr(userInput));
+    results.textContent = content;
 })
 
 threeBtn.addEventListener('click', () => {
     number = 3;
     userInput.push(number);
-    results.textContent = numberWithCommas(joinArr(userInput));
+    content = numberWithCommas(joinArr(userInput));
+    results.textContent = content;
 })
 
 fourBtn.addEventListener('click', () => {
     number = 4;
     userInput.push(number);
-    results.textContent = numberWithCommas(joinArr(userInput));
+    content = numberWithCommas(joinArr(userInput));
+    results.textContent = content;
 })
 
 fiveBtn.addEventListener('click', () => {
     number = 5;
     userInput.push(number);
-    results.textContent = numberWithCommas(joinArr(userInput));
+    content = numberWithCommas(joinArr(userInput));
+    results.textContent = content;
 })
 
 sixBtn.addEventListener('click', () => {
     number = 6;
     userInput.push(number);
-    results.textContent = numberWithCommas(joinArr(userInput));
+    content = numberWithCommas(joinArr(userInput));
+    results.textContent = content;
 })
 
 sevenBtn.addEventListener('click', () => {
     number = 7;
     userInput.push(number);
-    results.textContent = numberWithCommas(joinArr(userInput));
+    content = numberWithCommas(joinArr(userInput));
+    results.textContent = content;
 })
 
 eightBtn.addEventListener('click', () => {
     number = 8;
     userInput.push(number);
-    results.textContent = numberWithCommas(joinArr(userInput));
+    content = numberWithCommas(joinArr(userInput));
+    results.textContent = content;
 })
 
 nineBtn.addEventListener('click', () => {
     number = 9;
     userInput.push(number);
-    results.textContent = numberWithCommas(joinArr(userInput));
+    content = numberWithCommas(joinArr(userInput));
+    results.textContent = content;
 })
 
 zeroBtn.addEventListener('click', () => {
     number = 0;
     userInput.push(number);
-    results.textContent = numberWithCommas(joinArr(userInput));
+    content = numberWithCommas(joinArr(userInput));
+    results.textContent = content;
 })
- 
+
+decimalBtn.addEventListener('click', () => {
+
+})
+
+divideBtn.addEventListener('click', () => {
+   firstUserInput = content;
+   userInput.length = 0;
+})
+
+equalBtn.addEventListener('click', () => {
+secondUserInput = content;
+answer = divide(firstUserInput, secondUserInput);
+results.textContent = answer;
+})
+
 
 
 
